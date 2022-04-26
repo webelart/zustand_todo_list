839bf5567b9a336be05226dfe96f4d9ac5522487
Elena Litvinova <ivanovaev90@gmail.com>
2022-04-26 17:31:25 +0100

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './views/App';
+
+import './views/styles/reset.scss';
+import './views/styles/common.scss';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
---
+++ src/views/App/index.tsx
@@ -0,0 +1,57 @@
+import React, {useEffect} from 'react';
+
+import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
+import { InputTask } from '../components/InputTask';
+
+import styles from './index.module.scss';
+
+export const App: React.FC = () => {
+    const [
+        tasks,
+        createTask,
+        updateTask,
+        removeTask
+    ] = useToDoStore(state => [
+        state.tasks,
+        state.createTask,
+        state.updateTask,
+        state.removeTask,
+    ]);
+
+    useEffect(() => {
+        console.log(111)
+    }, [])
+
+    console.log(222)
+
+    return (
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>To Do App</h1>
+            <section className={styles.articleSection}>
+                <InputPlus
+                    onAdd={(title) => {
+                       if (title) {
+                            createTask(title)
+                       } 
+                    }}
+                />
+            </section>
+            <section className={styles.articleSection}>
+                {!tasks.length && (
+                    <p className={styles.articleText}>There is no one task.</p>
+                )}
+                {tasks.map((task) => (
+                    <InputTask
+                        key={task.id}
+                        id={task.id}
+                        title={task.title}
+                        onDone={removeTask}
+                        onEdited={updateTask}
+                        onRemoved={removeTask}
+                    />
+                ))}
+            </section>
+        </article>
+    );
+}
